4ad0ac426b13b9ebb96a6810d9e29f7fd5ce7629
curjlync <curjlync@ut.utm.edu>
2022-08-27 21:57:53 -0500

    adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author CJs-Desktop
+ * @date 2022-08-27
+ * @brief Hello world
+ * 
+ * something generic used to get used to working with git
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+  
+  return 0;
+}
