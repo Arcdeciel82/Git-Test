7e1eafffd6cdc842d85553c0150f0a0b9cbe6162
curjlync <curjlync@ut.utm.edu>
2022-08-28 11:36:45 -0500

    Added product feature

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 using namespace std;
 
 int Sum(int n);
+int Product(int n);
 
 int main() {
   cout << "Hello World!\n";
@@ -20,6 +21,7 @@ int main() {
   int num;
   cin >> num;
   cout << "Sum: " << Sum(num) << endl;
+  cout << "Product: " << Product(num) << endl;
   return 0;
 }
 
@@ -27,6 +29,14 @@ int Sum(int n) {
   int total = 0;
   for(int i = 1; i <= n; i++) {
     total += i;
+  }
+  return total;
+}
+
+int Product(int n) {
+  int total = 1;
+  for(int i = 1; i <= n; i++) {
+    total *= i;
   }
   return total;
 }
